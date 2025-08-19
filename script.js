7126cd9cd3ba89ac81bf179a9a3e86bc992b0e0e
Vivek Kalal <vivekkalal09@gmail.com>
2025-08-20 01:11:09 +0530

    final App

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-null
+const Joke_Container = document.getElementById("joke");
+const Joke_Button = document.getElementById("btn");
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+
+async function getJoke() {
+    Joke_Container.classList.remove("fade");
+    response = await fetch(url);
+    data = await response.json();
+    Joke_Container.innerText = data.joke;
+    Joke_Container.classList.add("fade");
+}
+
+btn.addEventListener("click", getJoke);
